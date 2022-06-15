f29ab975fca70c78329f4ea0c9642de7292fc761
Sunshaochen <805960031@qq.com>
2022-06-15 16:17:04 +0800

    ex2-6

---
+++ 2.py
@@ -0,0 +1,5 @@
+# A comment,this is so you can read your program later
+# anything after the # is ignored by python
+print("I could have code like this.")
+# 评论
+
---
+++ 3.py
@@ -0,0 +1,3 @@
+print("It is greater?",5 > -2)
+print("It is greater or equal?", 5 >= -2 )
+print(" It is less or equal?", 5 <= -2)
