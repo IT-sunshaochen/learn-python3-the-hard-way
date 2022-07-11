ac32695d079834f615310505fe567453272558ef
Sunshaochen <805960031@qq.com>
2022-07-11 15:19:11 +0800

    整理梳理相关文件

---
+++ base/10.py
@@ -0,0 +1,14 @@
+tabby_cat = "\t I'm tabbed in ."
+persian_cat = "I'm spilt\n on a line."
+backslash  = "I'm \\ a \\ cat ."
+fat_cat = """
+
+l'll do a list:
+\t * Cat food 
+\t * fishes
+
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash)
+print(fat_cat)
