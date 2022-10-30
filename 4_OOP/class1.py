3acda6b420dafd391dd46a95b136cfe0dcec6dc1
sunshaochen <805960031@qq.com>
2022-10-30 14:43:40 +0800

    10.30

--- 4_OOP/class1.py
+++ 4_OOP/class1.py
@@ -2,7 +2,7 @@
 Author: sunshaochen 805960031@qq.com
 Date: 2022-10-18 15:21:23
 LastEditors: sunshaochen 805960031@qq.com
-LastEditTime: 2022-10-30 14:21:01
+LastEditTime: 2022-10-30 14:43:29
 FilePath: \learn-python3-the-hard-way\4_OOP\class1.py
 Description: 
 '''
